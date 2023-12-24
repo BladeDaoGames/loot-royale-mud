05b59cbfdbc30a95dc2b44fec6535f847537ff62
gamegrief <gamedrift451@gmail.com>
2023-12-24 16:04:50 +0800

    feat: added owner button and refactored button class

--- packages/client/src/components/shared/Button.tsx
+++ packages/client/src/components/shared/Button.tsx
@@ -4,27 +4,46 @@ interface ButtonProps {
   onClick: () => void;
   children: React.ReactNode;
   className?: string;
+  disabled?: boolean;
 }
 
-export const Button = ({ onClick, children, className }: ButtonProps) => {
-  const ButtonHoverAnimation = {
-    scale: 1.05,
-    transition: {
-      type: "spring",
-      stiffness: 150,
-    },
+export const Button = ({
+  onClick,
+  children,
+  className = "",
+  disabled = false,
+}: ButtonProps) => {
+  const buttonHoverAnimation = disabled
+    ? undefined  
+    : {
+        scale: 1.05,
+        transition: {
+          type: "spring",
+          stiffness: 150,
+        },
+      };
+
+  const handleClick = () => {
+    if (!disabled) {
+      onClick();
+    }
   };
 
-  const buttonClass = `cursor-pointer ${className}`;
+  const buttonClass = `cursor-pointer ${className} ${disabled ? "opacity-50 cursor-not-allowed" : ""}`;
 
   return (
     <motion.div
-      whileHover={ButtonHoverAnimation}
-      onClick={onClick}
+      whileHover={buttonHoverAnimation}
+      onClick={handleClick}
       className={buttonClass}
+      // the bottom 3 is to deal with accessibility issues
+      role="button"
+      tabIndex={disabled ? -1 : 0}
+      aria-disabled={disabled}
     >
       {children}
     </motion.div>
   );
 };
+
 export default Button;
